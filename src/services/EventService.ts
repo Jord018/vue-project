ad96d3ed43b73bca0db203a8830e950e9149f1ac
Jord <jord018@users.noreply.github.com>
2026-07-11 10:09:21 +0700

    16.2

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: '[your mock server url]',
+  baseURL: 'https://my-json-server.typicode.com/Jord018/vue-project',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -3,15 +3,14 @@ import EventCard from '@/components/EventCard.vue'
 import category_organize from './../components/category_organize.vue'
 import type { Event } from '@/types'
 import { ref, onMounted } from 'vue'
-import axios from 'axios'
+import EventService from '@/services/EventService'
+
 
 const events = ref<Event[] | null>(null)
 
 onMounted(() => {
-  axios
-    .get('https://my-json-server.typicode.com/Jord018/vue-project/events')
+    EventService.getEvents()
     .then((response) => {
-      console.log(response.data)
       events.value = response.data
     })
     .catch((error) => {
